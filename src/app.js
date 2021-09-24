20ed6ca4bb8b861090e1ad9c30e70ecd5f0e9ed3
chongwei <chongwei215@gmail.com>
2021-09-24 18:37:51 +0800

    inital commit

--- src/app.js
+++ src/app.js
@@ -6,5 +6,5 @@ const app = express()
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Listening on PORT:${PORT}`)
+    console.log(`Listening on PORT:${PORT}`)
 })
---
+++ src/routes/auth.js
@@ -0,0 +1,8 @@
+const router = require('express').Router()
+
+router.post('/register', async (req, res) => {
+})
+
+router.post('/login', (req, res) => {
+	
+})
